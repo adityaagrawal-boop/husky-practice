1c6ab39096fab561f81d2be24693bb7f9fcf8864
adityaagrawal-boop <aditya.agrawal@webrexstudio.com>
2026-05-11 11:39:01 +0530

    prectice with more error in index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const greeting = 'Hello Didi';
-export { greeting };
+const user = { name: 'Drashti', age: 25, city: 'India' };
+function sayHi(name) {
+  return 'Hello ' + name;
+}
+const mess = sayHi(user?.name);
+console.log(mess);
